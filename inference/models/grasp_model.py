2fc4f7e14f7cfce80775fa62dea8ab138d15ab5f
trucchau171 <91869898+trucchau171@users.noreply.github.com>
2024-04-30 00:07:34 +0700

    Update codebase

--- inference/models/grasp_model.py
+++ inference/models/grasp_model.py
@@ -13,9 +13,12 @@ class GraspModel(nn.Module):
     def forward(self, x_in):
         raise NotImplementedError()
 
-    def compute_loss(self, xc, yc):
-        y_pos, y_cos, y_sin, y_width = yc
-        pos_pred, cos_pred, sin_pred, width_pred = self(xc)
+    def compute_loss(self, xc, yc, prompt=None):
+        y_pos, y_cos, y_sin, y_width = yc	      
+        if prompt is not None:
+            pos_pred, cos_pred, sin_pred, width_pred = self(xc, prompt)
+        else:
+            pos_pred, cos_pred, sin_pred, width_pred = self(xc)
 
         p_loss = F.smooth_l1_loss(pos_pred, y_pos)
         cos_loss = F.smooth_l1_loss(cos_pred, y_cos)
